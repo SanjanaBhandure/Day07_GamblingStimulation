63e336c46a3e7c0ca2316247bfad9a600f5233a2
SanjanaBhandure <sweetybhandure@gmail.com>
2022-04-18 22:17:41 +0530

    Added: As a Calculative Gambler if won or lost 50% of the stake, would resign for the day

--- GamblingStimulation.java
+++ GamblingStimulation.java
@@ -8,14 +8,15 @@ public class GamblingStimulation {
     /*
      * Static Variables STAKE_PER_DAY and BET_PER_GAME created
      */
-    public static final int STAKE_PER_DAY = 100;             // Stake per day
-    public static final int BET_PER_GAME = 1;                //Bet per game
+    public static final int STAKE_PER_DAY = 100;     // Stake per day
+    public static final int BET_PER_GAME = 1;        //Bet per game
     public static final int WIN = 1;
-    public static int stake = 0;                             //initial gambler bets with 1
+    public static int stake = 0;                     //Initial gambler bets with 1
 
     /*
      * winCheck method will check if gambler wins or loss the game
      */
+
     public static void winCheck() {
         Random random = new Random();
         int betReturns = random.nextInt(2);
@@ -28,13 +29,28 @@ public class GamblingStimulation {
             stake--;
             logger.info("You loss the Game!!!");
         }
-        logger.info("Stake: " + stake);
+        logger.info("Stake: " +stake);
+
+    }
+
+    /*
+     * As a Calculative Gambler if won or lost 50% of the stake, would resign for
+     * the day
+     */
+
+    public static void resignDayCheck() {
+        int totalStake = 0;
+        while (stake != 50 && stake != -50) {
+            winCheck();
+        }
+        totalStake = stake + STAKE_PER_DAY;
+        logger.info("Total stake for resign for a day is: " +totalStake);
+
     }
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
         logger.info("Welcome to Gambling Simulation");
-        //Calling winCheck method
-        winCheck();
+        resignDayCheck();
     }
 }
